9e8d1f57ec72f01f4cccb9a4a9793f225b53aa4c
Jonathan Gold <jonathan.e.gold@gmail.com>
2017-08-26 07:49:22 -0400

    more opcodes, time to start modeling ram

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,20 @@ fn read_byte_from_rom(counter:u16) -> u8 {
     
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct Cpu {
     //registers
     reg_pc: u16,
-    a:u8
+    a:u8,
+    b:u8,
+    c:u8,
+    d:u8,
+    e:u8,
+    f:u8,
+    g:u8,
+    h:u8,
+    l:u8,
+
 }
 
 impl Cpu {
@@ -40,7 +49,43 @@ impl Cpu {
                 self.reg_pc += 1;
                 self.process();
             },
-            _=> {panic!("{:#x}", byte)}
+            //ld hl
+            0x21 => {
+                let byte1 = read_byte_from_rom(self.reg_pc + 1);
+                let byte2 = read_byte_from_rom(self.reg_pc + 2);
+                self.h = byte2;
+                self.l = byte1;
+                self.reg_pc += 3;
+                self.process();
+            },
+            //ld c
+            0xe => {
+                let byte = read_byte_from_rom(self.reg_pc +1);
+                self.c = byte;
+                self.reg_pc += 2;
+                self.process();
+            },
+            //ld b
+            0x6 => {
+                let byte = read_byte_from_rom(self.reg_pc +1);
+                self.b = byte;
+                self.reg_pc += 2;
+                self.process();
+            },
+            //ld
+            0x32 => {
+                //load a into ff00+C
+                let byte1 = read_byte_from_rom(self.reg_pc + 1) as u16;
+                let byte2 = (read_byte_from_rom(self.reg_pc + 2) as u16) << 8;
+                let addr = byte2 + byte1;
+                //TODO: implement ram to run this
+                self.reg_pc += 3;
+                self.process();
+            }
+            _=> {
+                println!("{:?}", self);
+                panic!("{:#x}", byte);
+            }
         }
     }
 }
